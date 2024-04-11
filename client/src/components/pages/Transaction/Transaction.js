82616877c415cafbc19aa6779eb464f7f6b9992f
Nhu Pham <bog15@txstate.edu>
2024-04-11 14:16:26 -0500

    added transaction page

---
+++ client/src/components/pages/Transaction/Transaction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function Transaction() {
+  return (
+    <div> 
+        <h1>Transaction Page</h1>
+    </div>
+  )
+}
+
+export default Transaction;
