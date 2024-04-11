d82b5c92fb991531214e17a85d74a3446840aa6e
Phat Tran <lilphatti.tran@gmail.com>
2024-04-11 16:57:11 -0500

    fixed issues w/ routes
    *clicking on profile link works but still has default sidebar, keep and add profile on right?
    *sizing issue on home page

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,23 @@ import Profile from './components/pages/Profile/Profile';
 
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Dashboard/>} />
-          <Route path="/Profile" element={<Profile/>} />
-        </Routes>
-      </div>
-    </Router>
-    //<Dashboard /> // 1st page displayed when ran
 
+  function TestComponent() {
+    return <div>Test Route Component</div>;
+  } 
+
+  return (
+    <div className='container'>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/Profile" element={<Profile/>} />
+      </Routes>
+    </Router>
+
+    </div>
+    //<Dashboard /> // 1st page displayed when ran
 
     // <Router>
     //   <div>
