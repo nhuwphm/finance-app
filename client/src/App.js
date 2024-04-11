fb08f0ef60f4ad2e7d7b8e3d997674b773c6b65b
Phat Tran <lilphatti.tran@gmail.com>
2024-04-11 16:46:00 -0500

    new page and routing
    * issue with duplicate pages
    * try fix by using react-route-dom instead of windows.location.href

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,38 @@ import React from 'react';
 import Dashboard from "./components/pages/Dashboard/Dashboard";
 import Sidebar from './components/layout/Sidebar';
 
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Profile from './components/pages/Profile/Profile';  
+
+
 
 function App() {
   return (
-    <Dashboard/>
+    <Router>
+      <div>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Dashboard/>} />
+          <Route path="/Profile" element={<Profile/>} />
+        </Routes>
+      </div>
+    </Router>
+    //<Dashboard /> // 1st page displayed when ran
+
+
+    // <Router>
+    //   <div>
+    //     <Sidebar />
+    //     <Link to="/">Dashboard</Link>
+    //     <Link to="/Profile">Profile</Link>
+    //     <Routes>
+    //       <Route exact path="/" component={<Dashboard/>} />
+    //       <Route path="/Profile" component={<Profile/>} />
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 }
+
 
 export default App;
---
+++ client/src/components/pages/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <div className="profile-page">
+            <h1>This is the Profile Page</h1>
+        </div>
+    );
+}
+
+export default Profile;
