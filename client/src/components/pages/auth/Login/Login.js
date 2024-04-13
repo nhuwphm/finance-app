a7f4d62ecb12029033f41a068e52441e468ae976
Nhu Pham <bog15@txstate.edu>
2024-04-13 04:53:53 -0500

    added login page

--- client/src/components/pages/auth/Login/Login.js
+++ client/src/components/pages/auth/Login/Login.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
+import CustomButton from '../Button/CustomButton';
 
 const Login = () => {
     return(
@@ -19,6 +21,18 @@ const Login = () => {
                     name="password"
                     id="password"
                     test="input-pass-test" />
+
+                <CustomButton
+                    type="submit"
+                    title="Log in"
+                    test="btn-login-test"/>
+
+                <div className='link'>
+                    Don&apos;t have an account?
+                    <Link to="/signup" id="link-signup">
+                    Sign up
+                    </Link>
+                </div>
             </form>
 
         </div>
