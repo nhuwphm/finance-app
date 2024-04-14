ceb0c74a0254fb6045026b79b0db9e15c52def8d
Nhu Pham <bog15@txstate.edu>
2024-04-13 21:23:51 -0500

    integrated chatGPT

--- client/src/components/pages/Financial Help/FinancialHelp.js
+++ client/src/components/pages/Financial Help/FinancialHelp.js
@@ -2,14 +2,21 @@ import {React, useState} from 'react';
 import './FinancialHelp.css';
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
-import { PersistentCacheIndexManager } from 'firebase/firestore';
+
+
+
 
 function FinancialHelp() {
-  const [typing, setTyping] = useState (false);
+  const API_KEY = process.env.REACT_APP_OPEN_AI_API_KEY;
+const systemMessage = { 
+  "role": "system", "content": "Explain things like you're talking to a person with a basic knowledge of finance."
+}
+  const [isTyping, setIsTyping] = useState(false);
   const [messages, setMessages] = useState([
     {
       message: "Hello, I'm a virtual assistant of Smart Spender! I'm here to help you with your finances. Ask me anything!!",
       sender: "ChatGPT",
+      sentTime: "just now",
       direction:'incoming'
     }
   ]);
@@ -24,18 +31,57 @@ function FinancialHelp() {
     const newMessages = [...messages, newMessage];
     setMessages(newMessages);
 
-    setTyping(true);
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) { 
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message}
+    });
+  
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage, 
+        ...apiMessages 
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "ChatGPT"
+      }]);
+      setIsTyping(false);
+    });
   }
-    
-
-
   return (
     <div className='chat-container'> 
     <h1>Virtual Assistant</h1>
     <div className="messages-container">
       <MainContainer>
         <ChatContainer>
-          <MessageList typingIndicator= {typing ?<TypingIndicator content="SmartSpender is typing"/> : null}>
+          <MessageList 
+          scrollBehavior='smooth'
+          typingIndicator= {isTyping ?<TypingIndicator content="SmartSpender is typing"/> : null}>
 
             {messages.map((message,i) => {
               return <Message key={i} model={message} />
