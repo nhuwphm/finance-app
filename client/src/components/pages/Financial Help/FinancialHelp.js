ca83e90c90c2d6ef7e4a454a48b14a5e979b4911
Nhu Pham <bog15@txstate.edu>
2024-04-13 20:03:01 -0500

    added virtual assistant  for financial help page

--- client/src/components/pages/Financial Help/FinancialHelp.js
+++ client/src/components/pages/Financial Help/FinancialHelp.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import {React, useState} from 'react';
+import './FinancialHelp.css';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import { PersistentCacheIndexManager } from 'firebase/firestore';
 
 function FinancialHelp() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm a virtual assistant of Smart Spender! I'm here to help you with your finances. Ask me anything!!",
+      sender: "ChatGPT"
+    }
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message : message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+  }
+    
+
+
   return (
-    <div> 
-        Virtual Assistant Page
+    <div className='chat-container'> 
+    <h1>Virtual Assistant</h1>
+    <div className="messages-container">
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            {messages.map((message,i) => {
+              return <Message key={i} model={message} />
+            })}
+          </MessageList>
+          <MessageInput placeholder='Type message here' onSend={handleSend} />
+        </ChatContainer>
+      </MainContainer>
     </div>
+</div>
   )
 }
 
