400696f6e089722b34640455699679583e003557
Nhu Pham <bog15@txstate.edu>
2024-04-14 19:40:03 -0500

    sidebar

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -22,15 +22,15 @@ function Sidebar() {
 
   return (
    <div className='sidebar'>
-        <div class="user">
-            <div class="profile-pic">
-                <p class="time">Available Balance</p>
-                <p class="date">$2,041.05</p>
-    
+        <div className="user">
+            <div className="profile-pic">
+            <svg className="calendar-svg" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M14 0H2a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M6.5 7a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm-9 3a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm-9 3a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2zm3 0a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+            <p className="date">Wednesday, April 5th</p>
             </div>
-            <div class="user-info">
-                <p class="user-name">User 7B-3690</p>
-                <p class="name-title">Standard User</p>
+            <div className="time">12:15pm</div>
+            <div className="user-info">
+                <p className="user-name">User 7B-3690</p>
+                <p className="name-title">Standard User</p>
             </div>
             
         </div>
